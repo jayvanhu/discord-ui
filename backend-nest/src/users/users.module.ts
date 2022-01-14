787121d597572b8cb86d01b98af6ab2b2364f8b2
unknown <19579031+jayvanhu@users.noreply.github.com>
2022-01-13 19:14:00 -0800

    Add UserEntity to users module

--- backend-nest/src/users/users.module.ts
+++ backend-nest/src/users/users.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/data/user.entity';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
 
 @Module({
-	providers: [UsersService],
+	imports: [
+		TypeOrmModule.forFeature([UserEntity]),
+	],
+	providers: [UsersService, UsersResolver],
 	exports: [UsersService, UsersResolver],
 })
 export class UsersModule {}
