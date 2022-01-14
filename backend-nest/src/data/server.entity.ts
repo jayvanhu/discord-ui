c6c27e873242bdd34756feef01e7ea5471f9a96a
unknown <19579031+jayvanhu@users.noreply.github.com>
2022-01-13 19:16:04 -0800

    Add servers module

--- backend-nest/src/data/server.entity.ts
+++ backend-nest/src/data/server.entity.ts
@@ -11,6 +11,8 @@ export class ServerEntity {
 
 	// TODO should both owner and members map to user.servers for the relation?
 	// TODO can maybe try implementing an ownership transfer logic?
+	// Can have it appear as a prompt on the UI to select new owner?
+	// Maybe for owner leaving the server?
 	@ManyToOne(() => UserEntity, user => user.ownedServers, { onDelete: 'CASCADE' })
 	owner: UserEntity
 
